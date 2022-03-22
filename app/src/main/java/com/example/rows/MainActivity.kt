49d86a7ca5ceefbe37f44b16598f8e4b29258851
Zac Durber <zdurber@gmail.com>
2022-03-22 17:01:06 +0800

    Auth setup for email and google

--- app/src/main/java/com/example/rows/MainActivity.kt
+++ app/src/main/java/com/example/rows/MainActivity.kt
@@ -5,6 +5,10 @@ import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import java.io.*
@@ -13,34 +17,69 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(FirebaseAuthUIActivityResultContract()) {
+        res -> this.onSignInResult(res)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+            val recyclerView: RecyclerView = findViewById(R.id.recyclerViewMain)
+            recyclerView.layoutManager = LinearLayoutManager(this)
+            val data = ArrayList<MoodEntryModel>()
+
+            val jsonArray = loadFromJSONAsset()
+            val gson = GsonBuilder().create()
+            val moodEntries = gson.fromJson(jsonArray, Array<MoodEntryModel>::class.java).toList()
+            for(x in moodEntries.indices) {
+                data.add(moodEntries[x])
+            }
+
+            val adaptor = RecyclerViewAdaptor(data)
+            recyclerView.adapter = adaptor
+
+            val addNewButton: ImageButton = findViewById(R.id.addNewButton)
+            addNewButton.setOnClickListener {
+                val moodEntry = createNewMoodEntry()
+                data.add(moodEntry)
+                writeEntrytoFile(moodEntry)
+
+                adaptor.run {
+                    notifyDataSetChanged()
+                }
+            }
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            launchSignInEvent()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerViewMain)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        val data = ArrayList<MoodEntryModel>()
+        launchSignInEvent()
+    }
 
-        val jsonArray = loadFromJSONAsset()
-        val gson = GsonBuilder().create()
-        val moodEntries = gson.fromJson(jsonArray, Array<MoodEntryModel>::class.java).toList()
-        for(x in moodEntries.indices) {
-            data.add(moodEntries[x])
-        }
+    private fun launchSignInEvent() {
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
 
-        val adaptor = RecyclerViewAdaptor(data)
-        recyclerView.adapter = adaptor
-
-        val addNewButton: ImageButton = findViewById(R.id.addNewButton)
-        addNewButton.setOnClickListener {
-            val moodEntry = createNewMoodEntry()
-            data.add(moodEntry)
-            writeEntrytoFile(moodEntry)
-
-            adaptor.run {
-                notifyDataSetChanged()
-            }
-        }
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
     }
 
     fun writeEntrytoFile(moodEntryModel: MoodEntryModel) {
