c74b65f89175d6fed618a9a68a9c6f6ee8fde85b
Zac Durber <zdurber@gmail.com>
2022-03-22 12:47:05 +0800

    Setup for firebase integration

--- app/src/main/java/com/example/rows/MainActivity.kt
+++ app/src/main/java/com/example/rows/MainActivity.kt
@@ -5,14 +5,14 @@ import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import java.io.*
 import java.nio.charset.Charset
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,24 +34,45 @@ class MainActivity : AppCompatActivity() {
         val addNewButton: ImageButton = findViewById(R.id.addNewButton)
         addNewButton.setOnClickListener {
             val moodEntry = createNewMoodEntry()
+            data.add(moodEntry)
+            writeEntrytoFile(moodEntry)
 
             adaptor.run {
-                data.add(moodEntry)
                 notifyDataSetChanged()
             }
         }
     }
 
+    fun writeEntrytoFile(moodEntryModel: MoodEntryModel) {
+        var gson = Gson()
+        var jsonString: String = gson.toJson(moodEntryModel)
+        val fileout: FileOutputStream = openFileOutput("testData.json", MODE_PRIVATE)
+        val outwrite: OutputStreamWriter = OutputStreamWriter(fileout)
+        outwrite.append(jsonString)
+        outwrite.close()
+        fileout.close()
+    }
+
     fun loadFromJSONAsset(): String {
-        var json: String
-        println(assets)
-        val inputStream = assets.open("testData.json")
-        val size = inputStream.available()
-        val buffer = ByteArray(size)
-        val charset: Charset = Charsets.UTF_8
-        inputStream.read(buffer)
-        inputStream.close()
-        json = String(buffer, charset)
+        var json = ""
+
+        println("File to read: " + this.filesDir.absoluteFile)
+
+        val file = File("testData.json")
+        if (file.isFile) {
+            val fileReader: FileReader = FileReader("testData.json")
+            json = fileReader.readLines().toString()
+
+        }
+        else {
+            val inputStream = assets.open("testData.json")
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            val charset: Charset = Charsets.UTF_8
+            inputStream.read(buffer)
+            inputStream.close()
+            json = String(buffer, charset)
+        }
 
         return json
     }
