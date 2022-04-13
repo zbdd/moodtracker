461e79f9c0eca2c74ec9c1974ff2b6db358d8202
Zac Durber <zdurber@gmail.com>
2022-04-14 07:20:07 +0800

    Init commit of file import added

--- app/src/main/java/com/example/rows/SettingsActivity.kt
+++ app/src/main/java/com/example/rows/SettingsActivity.kt
@@ -1,20 +1,31 @@
 package com.example.rows
 
+
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Switch
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SettingsActivity() : AppCompatActivity() {
+
+    private lateinit var getImportJsonFileResult: ActivityResultLauncher<Intent>
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         val sMoodNumerals: Switch = findViewById(R.id.sMoodNumerals)
+        val tvSettingsImport: TextView = findViewById(R.id.tvSettingsImport)
         val bSettingsConfirm: Button = findViewById(R.id.bSettingsConfirm)
 
-        var settings = intent.getParcelableExtra<Settings>("Settings")
+        val settings = intent.getParcelableExtra<Settings>("Settings")
         sMoodNumerals.isChecked = settings?.mood_numerals?.equals("true") ?: false
 
         sMoodNumerals.setOnCheckedChangeListener { compoundButton, isChecked ->
@@ -23,11 +34,25 @@ class SettingsActivity() : AppCompatActivity() {
             }
         }
 
+        tvSettingsImport.setOnClickListener {
+            val intent = Intent()
+                .setType("*/*")
+                .setAction(Intent.ACTION_GET_CONTENT)
+
+            getImportJsonFileResult.launch(intent)
+        }
+
         bSettingsConfirm.setOnClickListener {
             val finishIntent = Intent()
             finishIntent.putExtra("Settings", settings)
             setResult(RESULT_OK, finishIntent)
             finish()
         }
+
+        getImportJsonFileResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                val data = it.data
+                println(data)
+            }
     }
 }
