56363d1918f513bcd563f1257bd74579974bad37
Zac Durber <zdurber@gmail.com>
2022-04-21 07:04:01 +0800

    Pre-Closed Release commit Images for Google Store New launcher Added in new activity to create moods - unused atm

---
+++ app/src/main/java/com/kalzakath/zoodle/createMoodEntryActivity.kt
@@ -0,0 +1,11 @@
+package com.kalzakath.zoodle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class createMoodEntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_mood_entry)
+    }
+}
