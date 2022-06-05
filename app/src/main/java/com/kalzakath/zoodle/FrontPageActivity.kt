a0c7d835618ffa070c4930962dba782df80db8b9
Zac Durber <zdurber@gmail.com>
2022-06-06 07:35:08 +0800

    New Front Page created

---
+++ app/src/main/java/com/kalzakath/zoodle/FrontPageActivity.kt
@@ -0,0 +1,11 @@
+package com.kalzakath.zoodle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FrontPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_front_page)
+    }
+}
