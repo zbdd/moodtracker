2d484d742c4dc637004895e6d7cb1acf86c05fd8
Zac Durber <zdurber@gmail.com>
2022-06-11 17:13:38 +0800

    Moodtest fixed for latest version of code

--- app/src/test/java/com/kalzakath/zoodle/MoodTest.kt
+++ app/src/test/java/com/kalzakath/zoodle/MoodTest.kt
@@ -10,7 +10,7 @@ class MoodTest {
         val mood = Mood()
         assertEquals("5", mood.value)
 
-        val mood_test_2 = Mood("1", 3)
+        val mood_test_2 = Mood("1")
         assertEquals("1", mood_test_2.value)
     }
 
@@ -19,22 +19,6 @@ class MoodTest {
         val mood = Mood()
         mood.value = "10"
         assertEquals("10", mood.value)
-    }
-
-    @org.junit.jupiter.api.Test
-    fun getMoodMode() {
-        val mood = Mood()
-        assertEquals(Mood.MOOD_MODE_FACES, mood.moodMode)
-    }
-
-    @org.junit.jupiter.api.Test
-    fun setMoodMode() {
-        val mood = Mood()
-        mood.moodMode = 123
-        // Setting moodMode will only accept defined values else returns -1
-        assertEquals(-1, mood.moodMode)
-        mood.moodMode = Mood.MOOD_MODE_FACES
-        assertEquals(Mood.MOOD_MODE_FACES, mood.moodMode)
     }
 
     @org.junit.jupiter.api.Test
